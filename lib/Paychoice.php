f031aecb6343accfa7883ca66aa22421fa07bd50
Ben Quinney <ben.quinney@gmail.com>
2019-08-18 10:27:04 +1000

    Updates used in Gym Mate
    Added private variable $bankTokenEndpoint
    Added private variable $refundEndpoint
    Added storeBank function
    Added chargeCardToken function
    Added chargeBankToken function
    Added refund function

--- lib/Paychoice.php
+++ lib/Paychoice.php
@@ -27,6 +27,8 @@ class Paychoice
 	private $chargeEndpoint = "api/v3/charge/";
     private $tokenEndpoint = "api/v3/token/";
     private $publicKeyEndpoint = "api/v3/publickey/";
+    private $bankTokenEndpoint = "api/v3/bank_token/";
+    private $refundEndpoint = "api/v3/refund/";
 	
 	private $httpClient;
 			
@@ -56,6 +58,16 @@ class Paychoice
         return $this->httpClient->post($this->tokenEndpoint, $data);	
     }
 
+    function storeBank($bankName, $bankBSB, $bankNumber)
+    {
+        $data = array(
+            "bank[name]" => $bankName,
+            "bank[bsb]" => $bankBSB,
+            "bank[number]" => $bankNumber,
+        );
+        return $this->httpClient->post($this->bankTokenEndpoint, $data);    
+    }
+
     function getPublicKey()
     {        
         return $this->httpClient->get($this->publicKeyEndpoint);	
@@ -75,6 +87,37 @@ class Paychoice
             "card_token" => $token,
         );
     	return $this->httpClient->post($this->chargeEndpoint, $data);	
+    }
+
+    function chargeCardToken($reference, $token, $currency, $amount)
+    {
+        $data = array(
+            "currency" => $currency,
+            "amount" => $amount,
+            "reference" => $reference,
+            "card_token" => $token,
+        );
+        return $this->httpClient->post($this->chargeEndpoint, $data);   
+    }
+    
+    function chargeBankToken($reference, $token, $currency, $amount)
+    {
+        $data = array(
+            "currency" => $currency,
+            "amount" => $amount,
+            "reference" => $reference,
+            "bank_token" => $token,
+        );
+        return $this->httpClient->post($this->chargeEndpoint, $data);   
+    }
+
+    function refund($reference, $charge_id, $amount){
+        $data = array(
+            "amount" => $amount,
+            "charge_id" => $charge_id,
+            "reference" => $reference
+        );
+        return $this->httpClient->post($this->refundEndpoint, $data);
     }
 
     function chargeCard($reference, $cardName, $cardNumber, $cardExpiryMonth, $cardExpiryYear, $cardCvv, $currency, $amount)
